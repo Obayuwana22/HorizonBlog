d4545622d3ffae62b740aa96e816bb2b90ca33c0
Obayuwana22 <successobayuwana@gmail.com>
2024-10-07 10:47:05 +0100

    fixed api error

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,10 +2,23 @@ import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
-    <div className="mt-[80px] ">
-      {/* Profile */}
-      <Link to="/sign up">Sign up</Link>
-    </div>
+    <section className="flex flex-col justify-center min-h-screen">
+      <div className=" border rounded-lg p-5 my-20 md:max-w-md md:mx-auto bg-white">
+        <div className="flex flex-col items-center">
+          <div className="rounded-full w-32 h-32 object-cover bg-gray-300">
+            <img src="" alt="Profile picture/avatar" />
+          </div>
+          <div className="text-center sm:text-left">
+            <div className="text-2xl font-bold">John Doe</div>
+            <p className="text-gray-500">Web Developer</p>
+          </div>
+          <div>
+            <button>Edit Profile</button>
+          </div>
+        </div>
+        <Link to="/sign up">Sign up</Link>
+      </div>
+    </section>
   );
 };
 export default Profile;
